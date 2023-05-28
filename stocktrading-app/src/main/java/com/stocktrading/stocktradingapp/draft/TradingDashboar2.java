2bd759f701806d642ac1702260b3fe7497b2044e
huiern214 <115102416+huiern214@users.noreply.github.com>
2023-05-28 15:16:01 +0000

    Updated

--- stocktrading-app/src/main/java/com/stocktrading/stocktradingapp/draft/TradingDashboar2.java
+++ stocktrading-app/src/main/java/com/stocktrading/stocktradingapp/draft/TradingDashboar2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.tradingdashboar2;
+package com.stocktrading.stocktradingapp.draft;
 import java.util.Scanner;
 /**
  *
--- stocktrading-app/src/main/java/com/stocktrading/stocktradingapp/draft/UserAuthenticationGUI.java
+++ stocktrading-app/src/main/java/com/stocktrading/stocktradingapp/draft/UserAuthenticationGUI.java
@@ -1,3 +1,5 @@
+package com.stocktrading.stocktradingapp.draft;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
